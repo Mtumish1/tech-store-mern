42d31b321d05534bf295295b2009d20b2dee0f9c
Mtumishi <blakhite@gmail.com>
2025-02-04 21:31:42 -0500

    models

---
+++ server/models/Product.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  brand: { type: String },
+}, { timestamps: true });
+
+export default mongoose.model('Product', productSchema);
