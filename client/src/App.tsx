4bec0981b8a8fa93baa3b920f905f9ba8a12e993
TheHopeson <blakhite@gmail.com>
2025-02-14 03:30:17 -0500

    day 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import Login from './components/Auth/Login';
+import ProductList from './Components/ProductList/ProductList';
+import Login from './Components/Auth/Login';
 
 const App = () => {
   return (
