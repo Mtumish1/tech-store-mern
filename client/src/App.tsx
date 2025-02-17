25913400535cdf648234d549ca739f5d7cd6cff5
TheHopeson <blakhite@gmail.com>
2025-02-17 15:05:48 -0500

    nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './Components/ProductList/ProductList';
 import Login from './Components/Auth/Login';
+import Home from './Components/Home/Home';
+import Register from './Components/Auth/Register';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Cart from './Components/Cart/Cart';
+import Checkout from './Components/Checkout/Checkout';
+import './App.css';
+
 
 const App = () => {
   return (
@@ -8,6 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Router>
   );
